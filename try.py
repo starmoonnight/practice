f2903bc2bcba6be516dd5cc88c712ce78ab47d6b
Tianfei Liu <tianfeiliu@gmail.com>
2018-01-01 20:56:17 -0600

    Created pig latin script

---
+++ try.py
@@ -0,0 +1,17 @@
+def main():
+    vowels = ['a','e','i','o','u']
+    sentence=input('Translate phrase into Pig Latin:')
+    sentence=sentence.split()
+    
+    for k in range(len(sentence)):
+        word = sentence[k]
+        if word[0] in vowels:
+            sentence[k] = word + 'way'
+        elif word[0] not in vowels:
+            sentence[k] = word[1:] + word[0]+ 'ay'
+        else:
+            sentence[k] = word
+    return ' '.join(sentence)
+if __name__=="__main__":
+    x=main()
+    print(x)
